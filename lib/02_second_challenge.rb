d0ece4174b55e301e12db9968f772fef4181ece4
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-30 23:47:06 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,10 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
+      patriarch: {}, 
+      matriarch: {},
+      hero: {}, 
+      hero_friends: [],
       }, 
     :capulet => {
-      #your key/value pairs here!
+      atriarch: {}, 
+      matriarch: {},
+      hero: {}, 
+      hero_friends: [],
+      
       }}
 
   
